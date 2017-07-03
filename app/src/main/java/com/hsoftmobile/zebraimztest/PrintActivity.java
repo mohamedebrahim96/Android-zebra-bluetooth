ceeb393244ff288936509a683e3383aff1ae9447
Mark Torres <mark.torres.mets@gmail.com>
2017-07-03 16:54:31 -0500

    App layout

---
+++ app/src/main/java/com/hsoftmobile/zebraimztest/PrintActivity.java
@@ -0,0 +1,13 @@
+package com.hsoftmobile.zebraimztest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrintActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_print);
+	}
+}
