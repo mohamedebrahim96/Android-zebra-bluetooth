4810971f3907963c5f910be96d851f6f979feb73
Mark Torres <mark.torres.mets@gmail.com>
2017-07-05 16:41:58 -0500

    Select printer from paired devices

--- app/src/main/java/com/hsoftmobile/zebraimztest/PrintActivity.java
+++ app/src/main/java/com/hsoftmobile/zebraimztest/PrintActivity.java
@@ -1,10 +1,17 @@
 package com.hsoftmobile.zebraimztest;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,16 +19,32 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.zebra.sdk.comm.Connection;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class PrintActivity extends AppCompatActivity {
 
-	public static final int RESULT_LOAD_IMAGE = 1;
+	public static final int REQ_LOAD_IMAGE = 1;
+	public static final int REQ_ENABLE_BT = 2;
+
+	private static final int BT_CLASS_PRINTER = 1664;
 
 	private ImageView imagePhoto;
 
-	private boolean photoLoaded;
+	private boolean photoLoaded, deviceHasBluetooth, bluetoothIsEnabled, printerFound;
+
+	private String printerAddress;
+
+	private BluetoothDevice selectedPrinter;
+
+	// BT stuff
+	private BluetoothAdapter bluetoothAdapter;
+	private Connection connection;
+	private ArrayList<BluetoothDevice> availablePrinters;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +53,22 @@ public class PrintActivity extends AppCompatActivity {
 
 		// var init
 		photoLoaded = false;
+		deviceHasBluetooth = false;
+		bluetoothIsEnabled = false;
+		printerFound = false;
+		printerAddress = "";
+
+		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (bluetoothAdapter != null) {
+			deviceHasBluetooth = true;
+			bluetoothIsEnabled = bluetoothAdapter.isEnabled();
+		}
 
 		// bind views
 		Button btnChoosePhoto = (Button) findViewById(R.id.btn_choose_photo);
 		Button btnPrintPhoto = (Button) findViewById(R.id.btn_print_photo);
 		Button btnPrintText = (Button) findViewById(R.id.btn_print_text);
+		Button btnPaired = (Button) findViewById(R.id.btn_paired);
 		final EditText textMessage = (EditText) findViewById(R.id.text_message);
 		imagePhoto = (ImageView) findViewById(R.id.image_photo);
 
@@ -52,7 +86,7 @@ public class PrintActivity extends AppCompatActivity {
 			@Override
 			public void onClick(View view) {
 				Intent loadPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-				startActivityForResult(loadPhoto, RESULT_LOAD_IMAGE);
+				startActivityForResult(loadPhoto, REQ_LOAD_IMAGE);
 			}
 		});
 		btnPrintPhoto.setOnClickListener(new View.OnClickListener() {
@@ -65,13 +99,23 @@ public class PrintActivity extends AppCompatActivity {
 				}
 			}
 		});
+		btnPaired.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (!deviceHasBluetooth) {
+					showToast("Your device does not have Bluetooth!");
+					return;
+				}
+				new PairedDevicesTask().execute();
+			}
+		});
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == RESULT_LOAD_IMAGE) {
+		if (requestCode == REQ_LOAD_IMAGE) {
 			if (resultCode == RESULT_OK && data != null) {
 				Uri uri = data.getData();
 				try {
@@ -90,9 +134,10 @@ public class PrintActivity extends AppCompatActivity {
 					ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 					resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
 					imagePhoto.setImageBitmap(resizedBitmap);
-					photoLoaded = true;
 					//*/
+					photoLoaded = true;
 				} catch (IOException e) {
+					showToast(e.getLocalizedMessage());
 					e.printStackTrace();
 				}
 			}
@@ -105,7 +150,105 @@ public class PrintActivity extends AppCompatActivity {
 		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 	}
 
+	private ProgressDialog createProgressDialog(String title, String message) {
+		ProgressDialog progressDialog = new ProgressDialog(this);
+		progressDialog.setTitle(title);
+		progressDialog.setMessage(message);
+		progressDialog.setIndeterminate(true);
+		progressDialog.setCancelable(false);
+		return progressDialog;
+	}
+
 	// iMZ Printer methods
+
+	private class PairedDevicesTask extends AsyncTask<Void, Void, String> {
+
+		private ProgressDialog pairedDialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+			pairedDialog = createProgressDialog("Getting list of paired devices", "Please wait...");
+			pairedDialog.show();
+
+			availablePrinters = new ArrayList<>();
+		}
+
+		@Override
+		protected String doInBackground(Void... voids) {
+			String errorMsg = "";
+			Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+			if (pairedDevices.size() > 0) {
+				for (BluetoothDevice device: pairedDevices) {
+					final String name = device.getName().trim();
+					final int majorDeviceClass = device.getBluetoothClass().getMajorDeviceClass();
+					final int deviceClass = device.getBluetoothClass().getDeviceClass();
+					if (majorDeviceClass == BluetoothClass.Device.Major.IMAGING && deviceClass == BT_CLASS_PRINTER) {
+						availablePrinters.add(device);
+					}
+				}
+				if (availablePrinters.size() == 0) {
+					errorMsg = "No paired printers found";
+				}
+			}
+			return errorMsg;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			pairedDialog.dismiss();
+			if (!result.isEmpty()) {
+				showToast(result);
+			} else {
+				if (availablePrinters.size() > 1) {
+					// TODO: show list of printers if more than one
+					showSelectPrinterDialog();
+				} else {
+					// set only printer found
+					selectedPrinter = availablePrinters.get(0);
+					printerFound = true;
+					showToast("Selected printer:" + selectedPrinter.getName());
+				}
+			}
+		}
+	}
+
+	private class PrintTextTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		@Override
+		protected String doInBackground(String... strings) {
+			return "";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+		}
+	}
+
+	private void showSelectPrinterDialog() {
+		if (availablePrinters == null || availablePrinters.size() == 0) return;
+		CharSequence[] printers = new CharSequence[availablePrinters.size()];
+		for (int i = 0; i < availablePrinters.size(); i++) {
+			printers[i] = availablePrinters.get(i).getName();
+		}
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Select a printer to use");
+		builder.setItems(printers, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int which) {
+				selectedPrinter = availablePrinters.get(which);
+				showToast("Selected printer:" + selectedPrinter.getName());
+			}
+		});
+	}
 
 	private void sendTextOverBluetooth(String message) {
 		showToast(message);
